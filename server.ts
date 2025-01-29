5a3a8c162f86b45ad3aeb699160eb314164c5f9c
ttejeda02 <ttejeda.rguez@gmail.com>
2025-01-29 14:12:13 -0600

    add: db connection

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import { Application, Router } from "oak";
+import { Client } from "mysql";
+import "@std/dotenv";
 
 const app = new Application();
 const router = new Router();
@@ -6,6 +8,16 @@ const router = new Router();
 router.get("/", (context) => {
   context.response.body = "Hello, cruel world!";
 });
+
+const client = await new Client().connect({
+  hostname: Deno.env.get("DB_HOST"),
+  username: Deno.env.get("DB_USER"),
+  db: Deno.env.get("DB_NAME"),
+  password: Deno.env.get("DB_PASSWORD"),
+});
+
+await client.execute(`USE ${Deno.env.get("DB_NAME")}`);
+console.log("Connected to the db");
 
 app.use(router.routes());
 app.use(router.allowedMethods());
