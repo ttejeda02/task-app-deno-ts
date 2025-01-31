6c446d9e41927df63d05471488616e6e062a0bab
ttejeda02 <ttejeda.rguez@gmail.com>
2025-01-31 00:11:51 -0600

    create: task api - get & post

---
+++ routes/task.ts
@@ -0,0 +1,33 @@
+import { Router } from "oak";
+import { createTask, getTasks } from "../models/TaskModel.ts";
+import { Task } from "../models/Task.ts";
+
+const taskRouter = new Router();
+
+taskRouter
+  .get("/tasks", async (context) => {
+    try {
+      const tasks = await getTasks();
+
+      context.response.status = 200;
+      context.response.body = tasks;
+    } catch (_error) {
+      context.response.status = 500;
+      context.response.body = { error: "Error fetching tasks" };
+    }
+  })
+  .post("/tasks", async (context) => {
+    try {
+      const body = await context.request.body.text();
+      const newTask: Task = JSON.parse(body);
+      const result = await createTask(newTask);
+
+      context.response.status = 201;
+      context.response.body = { message: "Task created", task: result };
+    } catch (_error) {
+      context.response.status = 400;
+      context.response.body = { error: "Error creating task" };
+    }
+  });
+
+export default taskRouter;
