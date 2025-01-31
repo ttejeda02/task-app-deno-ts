260e85a1cb4fad9666f931913ada0bccaa0a7295
ttejeda02 <ttejeda.rguez@gmail.com>
2025-01-31 11:54:53 -0600

    implemented: getById api

--- routes/task.ts
+++ routes/task.ts
@@ -1,5 +1,5 @@
 import { Router } from "oak";
-import { createTask, getTasks } from "../models/TaskModel.ts";
+import { createTask, getTaskById, getTasks } from "../models/TaskModel.ts";
 import { Task } from "../models/Task.ts";
 
 const taskRouter = new Router();
@@ -28,6 +28,18 @@ taskRouter
       context.response.status = 400;
       context.response.body = { error: "Error creating task" };
     }
-  });
+  })
+  .get("/tasks/:id", async (context) => {
+    try {
+      const { id } = context.params
+      const result = (await getTaskById(parseInt(id))).rows
+
+      context.response.status = 200
+      context.response.body = { message: "Task founded", task: result}
+    } catch (_error) {
+        context.response.status = 400;
+        context.response.body = { error: "Error getting task" }
+    }
+  })
 
 export default taskRouter;
