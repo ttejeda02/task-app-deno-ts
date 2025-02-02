245d3bf1ef6709f91598e099169f3d8707c89af5
ttejeda02 <ttejeda.rguez@gmail.com>
2025-02-02 14:14:48 -0600

    update: validate Task & implemented new responses

--- routes/task.ts
+++ routes/task.ts
@@ -16,7 +16,12 @@ taskRouter
       const tasks = await getTasks();
 
       ctx.response.status = 200;
-      ctx.response.body = tasks;
+      if (tasks.length == 0) {
+        ctx.response.body = { tasks: tasks };
+        return;
+      }
+
+      ctx.response.body = { message: "Tasks found", tasks: tasks };
     } catch (_error) {
       ctx.response.status = 500;
       ctx.response.body = { error: "Error fetching tasks" };
@@ -25,11 +30,27 @@ taskRouter
   .post("/tasks", async (ctx) => {
     try {
       const body = await ctx.request.body.text();
+
       const newTask: Task = JSON.parse(body);
+      const validateTaskResult = validateTask(newTask);
+
+      if (validateTaskResult) {
+        ctx.response.status = 400;
+        ctx.response.body = { message: validateTaskResult };
+        return;
+      }
+
       const result = await createTask(newTask);
+      if (result.lastInsertId == undefined) {
+        ctx.response.status = 500;
+        ctx.response.body = { error: "Internal server error" };
+        return;
+      }
+
+      const task = (await getTaskById(result.lastInsertId)).rows;
 
       ctx.response.status = 201;
-      ctx.response.body = { message: "Task created", task: result };
+      ctx.response.body = { message: "Task created", task: task };
     } catch (_error) {
       ctx.response.status = 400;
       ctx.response.body = { error: "Error creating task" };
@@ -38,7 +59,13 @@ taskRouter
   .get("/tasks/:id", async (ctx) => {
     try {
       const { id } = ctx.params;
+
       const result = (await getTaskById(parseInt(id))).rows;
+      if (result?.length == 0) {
+        ctx.response.status = 404;
+        ctx.response.body = { message: `Task with ID ${id} not found` };
+        return;
+      }
 
       ctx.response.status = 200;
       ctx.response.body = { message: "Task found", task: result };
@@ -51,11 +78,26 @@ taskRouter
     try {
       const { id } = ctx.params;
       const body = await ctx.request.body.text();
-      const newInfoTask: Task = JSON.parse(body);
-      const result = await updateTask(parseInt(id), newInfoTask);
 
-      ctx.response.body = 200;
-      ctx.response.body = { message: "Task updated", task: result };
+      const newInfoTask: Task = JSON.parse(body);
+      const validateTaskResult = validateTask(newInfoTask);
+      if (validateTaskResult) {
+        ctx.response.status = 400;
+        ctx.response.body = { message: validateTaskResult };
+        return;
+      }
+
+      const result = await updateTask(parseInt(id), newInfoTask);
+      if (result.affectedRows == 0) {
+        ctx.response.status = 404;
+        ctx.response.body = { message: "Task not found" };
+        return;
+      }
+
+      const task = (await getTaskById(parseInt(id))).rows;
+
+      ctx.response.status = 200;
+      ctx.response.body = { message: "Task updated", task: task };
     } catch (_error) {
       ctx.response.status = 400;
       ctx.response.body = { message: "Error updating task" };
@@ -66,12 +108,39 @@ taskRouter
       const { id } = ctx.params;
       const result = await deleteTask(parseInt(id));
 
-      ctx.response.body = 200;
-      ctx.response.body = { message: "Task deleted", task: result };
+      if (result.affectedRows == 0) {
+        ctx.response.status = 404;
+        ctx.response.body = { message: "Task not found" };
+        return;
+      }
+
+      ctx.response.status = 200;
+      ctx.response.body = { message: "Task deleted", taskId: id };
     } catch (_error) {
       ctx.response.status = 400;
       ctx.response.body = { message: "Error deleting task" };
     }
   });
 
+function validateTask(task: Task): string | null {
+  //Validate title
+  if (!task.title || task.title.trim() == "") {
+    return "Title is required";
+  }
+
+  //Validate status
+  if (
+    task.status &&
+    !["pending", "ongoing", "completed", "archived"].includes(task.status)
+  ) {
+    return "Invalid status value. Use 'pending', 'ongoing', 'completed' or 'archived'";
+  }
+
+  //Validate priority
+  if (task.priority && !["low", "medium", "high"].includes(task.priority)) {
+    return "Invalid priority value. Use 'low', 'medium' or 'high'";
+  }
+
+  return null;
+}
 export default taskRouter;
