c6ca7c3be53eed907d15dadaa55f7e2fff989953
ttejeda02 <ttejeda.rguez@gmail.com>
2025-02-04 12:57:11 -0600

    remove: unnecessary db instance

--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
 import { Application, Router } from "oak";
-import { Client } from "mysql";
 import taskRouter from "./routes/task.ts";
 import "@std/dotenv";
 
@@ -9,16 +8,6 @@ const router1 = new Router();
 router1.get("/", (context) => {
   context.response.body = "Hello, cruel world!";
 });
-
-const client = await new Client().connect({
-  hostname: Deno.env.get("DB_HOST"),
-  username: Deno.env.get("DB_USER"),
-  db: Deno.env.get("DB_NAME"),
-  password: Deno.env.get("DB_PASSWORD"),
-});
-
-await client.execute(`USE ${Deno.env.get("DB_NAME")}`);
-console.log("Connected to the db");
 
 app.use(router1.routes());
 app.use(router1.allowedMethods());
